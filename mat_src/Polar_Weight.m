92313dedbce93f1a9ae9f5893f2a7d3aeb4ece80
HL_1995 <347142093@qq.com>
2019-12-16 10:36:37 +0800

    add polarization-wight encoder
    Signed-off-by: HL_1995 <347142093@qq.com>

---
+++ mat_src/Polar_Weight.m
@@ -0,0 +1,14 @@
+function  PW  = Polar_Weight( N )
+%POLAR_WEIGHT 输入信道数，输出基于PW公式的可靠度序列
+    
+    PW = zeros(1,N);
+    bin_vec = dec2binvector( 0:1:(N-1), log2(N) );
+    
+    for ii= 1:1:N
+        for jj = 0:1:log2(N)-1
+            PW(1,ii) = PW(1,ii) + bin_vec(ii,log2(N)-jj) * 2^(jj/4);
+        end
+    end
+
+end
+
--- mat_src/encoder_polar4BEC.m
+++ mat_src/encoder_polar4BEC.m
@@ -16,8 +16,6 @@ frozen = zeros(1,F);             %固定位比特，规定全为0
 
 Bhat = Bhat_para(p, n-1);
 
-
-
 [Z_in_order,index] = sort( Bhat );          %将巴氏参数从小到大排列
 signal_index = sort( index( 1:S ) );        %前S位作为信息位
 frozen_index = sort( index( S+1:end ) );    %后面的作为冻结位
