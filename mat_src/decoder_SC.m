12ef01e840c8fe36b4597b302d248e605e96e8c5
HLhello <347142093@qq.com>
2020-03-05 23:25:13 +0800

    update wirk log
    Signed-off-by: HLhello <347142093@qq.com>

--- mat_src/decoder_SC.m
+++ mat_src/decoder_SC.m
@@ -1,4 +1,4 @@
-function [ C_hat ] = decoder_SC( R, channel_info, llr)
+function [ u_hat ] = decoder_SC( R, channel_info, llr)
 % 串行抵消译码算法 Successive Cancellation
 % 输入：码率R，信道信息channel_info，接收软信息llr
 % 输出：估计码字 C_hat
@@ -6,6 +6,7 @@ function [ C_hat ] = decoder_SC( R, channel_info, llr)
 frozen_index = find(channel_info==0);
 info_index = find(channel_info==1); 
 
+%initial llr
 
 
 
--- mat_src/f_func.m
+++
@@ -1,5 +0,0 @@
-
-f = log((1+exp(a+b))/(exp(a)+exp(b)));
-g = (-1)^uhat*a + b;
-
-
