7c69d02aa896ea4f917005c9588523d3b6a1b47f
HLhello <347142093@qq.com>
2020-05-07 22:24:22 +0800

    update work log
    Signed-off-by: HLhello <347142093@qq.com>

--- mat/scl/recursivelyUpdataB.m
+++ mat/scl/recursivelyUpdataB.m
@@ -1,4 +1,16 @@
-function xxx = recursivelyUpdataB(lambda,phi)
-
+function recursivelyUpdataB(lambda,phi,m)
+    
+    Beta = zeros();
+    PHI = floor(phi/2);
+    for bt = 0:1:(2^(m-lambda-1))
+        Beta
+    end
+    
+    if(mod(PHI,2) == 1)
+        recursivelyUpdataB(lambda-1,PHI,m)
+    end
+    
+    
+    
 
 end
