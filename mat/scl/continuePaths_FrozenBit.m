0739ea6c96edf66055c26c8da8fb851fc3df6b0d
HLhello <347142093@qq.com>
2020-05-10 22:52:27 +0800

    nothing

--- mat/scl/continuePaths_FrozenBit.m
+++ mat/scl/continuePaths_FrozenBit.m
@@ -3,6 +3,8 @@ function xxx = continuePaths_FrozenBit(phi)
 		if(pathIndexInactive(l)
 			continue
 			end
+			
+			
 		c_m = getArrayPointer(m,l)
 		set c_m(0,(phi mod 2)) to the frozen value of index phi
 	end
